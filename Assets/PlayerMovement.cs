40e6689eca5eb6fbd5a2b3547232ee30b1131fe5
Roxy Fernandez <5020021@stu.highlineschools.org>
2026-02-23 13:24:48 -0800

    fgjdsru
    dtiujdu

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    float horizontMovement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
