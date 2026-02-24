96424781a818dc91c65df7f4b5006fec240cc257
Roxy Fernandez <5020021@stu.highlineschools.org>
2026-02-24 13:25:14 -0800

    ftyifdtjdftjdfj

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
 
-    float horizontMovement;
+    float horizontalMovement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
     }
 }
