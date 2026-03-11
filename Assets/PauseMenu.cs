8845da69072555a62149b85a23849884e5668b33
Roxy Fernandez <5020021@stu.highlineschools.org>
2026-03-11 13:25:53 -0700

    push...
    p

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -8,21 +8,25 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void Resume()
     {
-
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void Menu()
     {
-
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 
     public void Restart()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
