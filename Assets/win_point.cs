be2ff1980557a63b68440e7af5f7190638fd58f8
Roxy Fernandez <5020021@stu.highlineschools.org>
2026-03-16 13:23:22 -0700

    woah

---
+++ Assets/win_point.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win_point : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(2);
+        }
+    }
+}
